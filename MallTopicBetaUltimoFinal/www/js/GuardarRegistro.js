b729bc66cebf01c858c5867ed3d60df152be4705
ALSephirot <asephirotammale@gmail.com>
2014-11-24 14:21:06 -0500

    Revert "Revert "hola""
    This reverts commit 116d0009e0525577e35addc83210f5a0642021b0.

--- MallTopicBetaUltimoFinal/www/js/GuardarRegistro.js
+++ MallTopicBetaUltimoFinal/www/js/GuardarRegistro.js
@@ -98,7 +98,7 @@ function captura(){
 			arte:ValidarGustos("Arte"),
 			musica:ValidarGustos("Música"),
 			turismo:ValidarGustos("Turismo"),
-			idcelular: LocalStorage.getItem("IdCelular")
+			idcelular: localStorage.getItem("IdCelular")
 	    };
 	
 	InsertRegis('regis_users', DatosUsuarioManual);
