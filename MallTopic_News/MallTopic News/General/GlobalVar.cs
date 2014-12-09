a28c871828cdbc2b37859985899c648b284bfa23
CamiloMT95 <camilomontoya95@gmail.com>
2014-12-09 15:47:20 -0500

    MallTopic News

---
+++ MallTopic_News/MallTopic News/General/GlobalVar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MallTopic_News.General
+{
+    public class GlobalVar
+    {
+        //public string WebApi = "http://localhost:1509/api/";
+        public string WebApi = "http://webapimalltopicnews.azurewebsites.net/api/";
+    }
+}
